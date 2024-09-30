f21c3bfc0f8eaecafcc81ae880f65fa38e2c2476
Priyanshu Mewada <oppriyanshu@gmail.com>
2024-09-30 13:00:10 +0530

    DONE

--- program1.py
+++ program1.py
@@ -1,6 +1,31 @@
+from typing import List
+
 class Solution:
-   
+    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+       #    write your code here
+        
+        if len(grid) == 0:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        island_count = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 'W' or (r, c) in visited:
+                return
+            visited.add((r, c))
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 'L' and (r, c) not in visited:
+                    dfs(r, c)
+                    island_count += 1
+
+        return island_count
--- program2.py
+++ program2.py
@@ -2,4 +2,20 @@ def decode_message( s: str, p: str) -> bool:
 
 # write your code here
   
+       def match(i, j):
+        if i >= len(s) and j >= len(p):
+            return True
+        if j >= len(p):
+            return False
+        
+        if p[j] == '*':
+            if match(i, j + 1):
+                return True
+            if i < len(s) and match(i + 1, j):
+                return True
+        if i < len(s) and (p[j] == s[i] or p[j] == '?'):
+            return match(i + 1, j + 1)
+        
         return False
+    
+       return match(0, 0)
